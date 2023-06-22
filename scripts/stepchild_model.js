cc0173fff6deb339e69000d3f9b1e0c96d76aaeb
alexlafetra <alexlafetrathompson@gmail.com>
2023-06-22 14:03:12 -0700

    added pages for tissue migration animation

--- scripts/stepchild_model.js
+++ scripts/stepchild_model.js
@@ -4,12 +4,13 @@ const maxScale = 2;
 
 function setup() {
   createCanvas(475,450, WEBGL);
-  stl = loadModel('../3d_models/test.stl');
+  // need a better path for this
+  stl = loadModel('3d_models/test.stl');
   currentScale = windowWidth/300;
 }
 
 function draw() {
-  background(255);
+  background(255,255,255,0);
   scale(currentScale>maxScale?maxScale:currentScale);
   rotateX(PI/2);
   rotateX(frameCount*0.01);
