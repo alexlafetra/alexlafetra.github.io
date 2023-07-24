95228c47feead21d51b27da7cbd147ea0a1535b2
alexlafetra <alexlafetrathompson@gmail.com>
2023-07-24 12:59:16 -0700

    added skye's album art

---
+++ tillthewatersrise/shader.js
@@ -0,0 +1,46 @@
+const vert = `
+precision lowp float;
+attribute vec3 aPosition;
+attribute vec2 aTexCoord;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+varying vec2 vVertTexCoord;
+
+void main(void) {
+  vec4 positionVec4 = vec4(aPosition, 1.0);
+  gl_Position = uProjectionMatrix * uModelViewMatrix * positionVec4;
+  vVertTexCoord = aTexCoord;
+}
+`;
+
+const frag = `
+precision lowp float;
+varying vec2 vVertTexCoord;
+
+uniform sampler2D source;
+uniform float noiseSeed;
+uniform float noiseAmount;
+
+// Noise functions
+// https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
+float rand(vec2 n) { 
+  return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+}
+
+void main() {
+  // GorillaSun's grain algorithm
+  vec4 inColor = texture2D(source, vVertTexCoord);
+  if(inColor.y >= 1.0 && inColor.x >= 1.0 && inColor.z >= 1.0){
+    gl_FragColor = inColor;
+    return;
+  }
+  gl_FragColor = clamp(inColor + vec4(
+    mix(-noiseAmount, noiseAmount, fract(noiseSeed + rand(vVertTexCoord * 1234.5678))),
+    mix(-noiseAmount, noiseAmount, fract(noiseSeed + rand(vVertTexCoord * 876.54321))),
+    mix(-noiseAmount, noiseAmount, fract(noiseSeed + rand(vVertTexCoord * 3214.5678))),
+    0.
+  ), 0., 1.);
+}
+`;
