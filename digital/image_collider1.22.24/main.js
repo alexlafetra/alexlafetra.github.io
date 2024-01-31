9c80ad5ab51a0eee95bbe8ac3dcc4682860c7990
alexlafetra <alexlafetrathompson@gmail.com>
2024-01-30 19:11:34 -0800

    added some things

--- digital/image_collider1.22.24/main.js
+++ digital/image_collider1.22.24/main.js
@@ -1,7 +1,11 @@
 /*
 Idea is to "throw" an image at another image and embed its pixels
 Pixels travel based on their velocity, which could be randomly encoded or taken from the RGBA data
+
+This could be implemented with a shader! You would need to do a similar "send array of 100 verts" and then link the vertex id
+to the frag shader
 */
+
 
 let mainCanvas;
 let projectileImage;
@@ -11,7 +15,7 @@ let velScale = 1;
 
 function preload(){
   projectileImage = loadImage('img2.png');
-  targetImage = loadImage('img2.png');
+  targetImage = loadImage('img3.png');
 }
 
 function getLuminance(c){
@@ -23,28 +27,27 @@ function collide(bullet,target,result){
   bullet.loadPixels();
   target.loadPixels();
   result.loadPixels();
+  result.pixels.fill(0);
   for(let x = 0; x<bullet.width; x++){
     for(let y = 0; y<bullet.height; y++){
       for(let i = 0; i<d; i++){
         for(let j = 0; j<d; j++){
           let bulletIndex = 4 * ((y * d + j) * bullet.width * d + (x * d + i)); 
-          // bulletIndex%=bullet.pixels.length;
           let targetIndex = 4 * ((y * d + j) * target.width * d + (x * d + i)); 
-          // targetIndex%=target.pixels.length;
           let c = [bullet.pixels[bulletIndex],bullet.pixels[bulletIndex+1],bullet.pixels[bulletIndex+2],bullet.pixels[bulletIndex+3]];
           let c2 = [target.pixels[targetIndex],target.pixels[targetIndex+1],target.pixels[targetIndex+2],target.pixels[targetIndex+3]];
 
           let x2 = x+velScale*c[2];
-          // let x2 = x+velScale*getLuminance(c);
+          // console.log(getLuminance(c));
           let resultIndex = 4 * ((y * d + j) * result.width * d + (x2 * d + i)); 
-          result.pixels[resultIndex] = c[0];
-          result.pixels[resultIndex+1] = c[1];
-          result.pixels[resultIndex+2] = c[2];
-          result.pixels[resultIndex+3] = c[3];
           // result.pixels[resultIndex] = (c[0]+c2[0])/2;
           // result.pixels[resultIndex+1] = (c[1]+c2[1])/2;
           // result.pixels[resultIndex+2] = (c[2]+c2[2])/2;
-          // result.pixels[resultIndex+2] = (c[3]+c2[3])/2;
+          // result.pixels[resultIndex+3] = (c[3]+c2[3])/2;
+          result.pixels[resultIndex] = (c[0]+c2[0]);
+          result.pixels[resultIndex+1] = (c[1]+c2[1]);
+          result.pixels[resultIndex+2] = (c[2]+c2[2]);
+          result.pixels[resultIndex+3] = (c[3]+c2[3]);
         }
       }
     }
@@ -73,21 +76,21 @@ function collide_slow(bullet,target,result){
   result.updatePixels();
 }
 
+let velSlider;
+
 function setup(){
-  // mainCanvas = createCanvas(800,1200,WEBGL);
-  mainCanvas = createCanvas(projectileImage.width,projectileImage.height,WEBGL);
+  velSlider = createSlider(-2,2,1,0.01);
+  mainCanvas = createCanvas(projectileImage.width*2,projectileImage.height*2,WEBGL);
   pixelDensity(1);
-  resultImage = createImage(400,400);
-  collide(projectileImage,targetImage,resultImage);
+  resultImage = createImage(width,height/2);
 }
 
 function draw(){
   background(0);
-  // velScale = mouseX/width;
+  velScale = velSlider.value();
   // console.log(velScale);
-
-  // collide_slow(projectileImage,targetImage,resultImage);
   collide(projectileImage,targetImage,resultImage);
-  image(targetImage,-width/2,-height/2,width,height);
-  image(resultImage,-width/2,-height/2,width,height);
+  image(projectileImage,-width/2,-height/2,width/2,height/2);
+  image(targetImage,0,-height/2,width/2,height/2);
+  image(resultImage,-width/2,0,width,height/2);
 }
