07fb0d4bf20b6fbec37c277748a69b5d0f7139c8
alexlafetra <alexlafetrathompson@gmail.com>
2024-06-21 23:42:21 -0700

    Add gentrificationflowfield

---
+++ gentrificationflowfield/main.js
@@ -0,0 +1,317 @@
+let flowField;
+let flowFields = [];
+let holcTexture;
+let mask;
+let presetFlowMask;
+
+let attractors = [];
+let repulsors = [];
+let gl;
+let mainCanvas;
+let tractOutlines;
+let idBuffer;
+let ids;
+
+
+//Presets
+let whiteComparisonPreset;
+let blackComparisonPreset;
+let asianComparisonPreset;
+let whiteProportionComparisonPreset;
+let blackProportionComparisonPreset;
+let asianProportionComparisonPreset;
+let whiteRatioPreset;
+let blackRatioPreset;
+let asianRatioPreset;
+let rentBurdenPreset;
+
+let presets;
+
+// Look for moving/migration data
+
+//20 is a good base number
+let NUMBER_OF_ATTRACTORS = 300;
+// let NUMBER_OF_ATTRACTORS = 20;
+const NUMBER_OF_FIELDS = 1;
+
+//controls whether or not the sim will load with prerendered data/choropleths
+//or with the full dataset, allowing you to explore/experiment
+// let devMode = true;
+let devMode = false;
+
+class DemographicVis{
+    constructor(title,description,data){
+        this.title = title;
+        this.description = description;
+        this.demographicFunction = data;
+    }
+    setActive(index,ff){
+        ff.chartTitle.html(this.title);
+        ff.chartEquation.html(this.description);
+        ff.presetIndex = index;
+        ff.calculateAttractors(NUMBER_OF_ATTRACTORS,this.demographicFunction);
+        ff.updateFlow();
+    }
+}
+
+class Preset{
+    constructor(title,description,map,aPoints,rPoints){
+        this.title = title;
+        this.description = description;
+        this.mapIndex = map;
+        this.attractors = aPoints;
+        this.repulsors = rPoints;
+    }
+    setActive(index,ff){
+        ff.chartTitle.html(this.title);
+        ff.chartEquation.html(this.description);
+        ff.presetIndex = index;
+        ff.setPresetAttractors();
+        ff.updateFlow();
+    }
+}
+function saveFlowField(){
+    saveCanvas(flowField.flowFieldTexture,'flowfield.png','png');
+}
+function saveChoropleth(){
+    saveCanvas(flowField.mapTexture, 'choropleth.png','png');
+}
+function saveMask(){
+    saveCanvas(flowField.particleMask, 'flowFieldMask.png','png');
+}
+function saveTracts(){
+    saveJSON(bayTracts,"tracts.json");
+}
+function saveOutlines(){
+    saveCanvas(tractOutlines, 'flowFieldMask.png','png');
+}
+
+function loadPresetMaps(){
+    presetFlowMask = loadImage("data/Prerendered/flowFieldMask.png");
+    tractOutlines = loadImage("data/Prerendered/tractOutlines.png");
+    holcTexture = loadImage("data/Prerendered/HOLC_Red.png");
+}
+
+function logAttractorsDevmode(){
+    let count = 0;
+    let string;
+    for(let i of presets){
+        i.setActive(count,flowField);
+        string += flowField.logFlowFieldData("preset"+count);
+        count++;
+    }
+    console.log(string);
+}
+
+function preload(){
+    if(devMode)
+        loadCensusCSVData();
+    else
+        loadPresetMaps();
+}
+
+//20 is about the limit
+function gatherDemographicMaxMins(n){
+    let points = getSignificantPoints(n);
+    points = points.concat(getLeastSignificantPoints(n));
+    return points;
+}
+
+
+function randomColor(){
+    return color(random(0,255),random(0,255),random(0,255));
+}
+
+function setup_DevMode(){
+    //Preset color/flows
+    whiteComparisonPreset = new DemographicVis("Change in White Population",
+                                            "P<sub>White 2020</sub> - P<sub>White 2000</sub>",whitePeopleChange);
+    blackComparisonPreset = new DemographicVis("Change in Black Population",
+                                                "P<sub>Black 2020</sub> - P<sub>Black 2000</sub>",blackPeopleChange);
+    asianComparisonPreset = new DemographicVis("Change in Asian Population",
+                                                "P<sub>Asian 2020</sub> - P<sub>Asian 2000</sub>",asianPeopleChange);
+    whiteProportionComparisonPreset = new DemographicVis("Change In Proportion of Population Identifying as White",
+                                            "P<sub>White 2000</sub> / P<sub>Total 2000</sub> - P<sub>White 2020</sub> / P<sub>Total 2020</sub>",proportionalWhiteChange);
+    blackProportionComparisonPreset = new DemographicVis("Change In Proportion of Population Identifying as Black",
+                                            "P<sub>Black 2000</sub> / P<sub>Total 2000</sub> - P<sub>Black 2020</sub> / P<sub>Total 2020</sub>",proportionalBlackChange);
+    asianProportionComparisonPreset = new DemographicVis("Change In Proportion of Population Identifying as Asian",
+                                            "P<sub>Asian 2000</sub> / P<sub>Total 2000</sub> - P<sub>Asian 2020</sub> / P<sub>Total 2020</sub>",proportionalAsianChange);
+    rentBurden1= new DemographicVis("Renters spending less than 10% of monthly income","idk",rentBurdenLessThan10);
+    rentBurden2= new DemographicVis("Renters spending 10-14% of monthly income","idk",rentBurden10to14);
+    rentBurden3= new DemographicVis("Renters spending 15-19% of monthly income","idk",rentBurden15to19);
+    rentBurden4= new DemographicVis("Renters spending 20-24% of monthly income","idk",rentBurden20to24);
+    rentBurden5= new DemographicVis("Renters spending 25-29% of monthly income","idk",rentBurden25to29);
+    rentBurden6= new DemographicVis("Renters spending 30-34% of monthly income","idk",rentBurden30to34);
+    rentBurden7= new DemographicVis("Renters spending 35-39% of monthly income","idk",rentBurden35to39);
+    rentBurden8= new DemographicVis("Renters spending 40-49% of monthly income","idk",rentBurden40to49);
+    rentBurden9= new DemographicVis("Renters spending more than 50% of monthly income","idk",rentBurden50orMore);
+
+    presets = [
+        whiteProportionComparisonPreset,
+        blackProportionComparisonPreset,
+        asianProportionComparisonPreset,
+        whiteComparisonPreset,
+        blackComparisonPreset,
+        asianComparisonPreset,
+        rentBurden1,
+        rentBurden2,
+        rentBurden3,
+        rentBurden4,
+        rentBurden5,
+        rentBurden6,
+        rentBurden7,
+        rentBurden8,
+        rentBurden9
+    ];
+
+    //parsing data and attaching it to tract geometry
+    setupMapData();
+
+    //setting the offsets so that the first point in the first shape is centered
+    let samplePoint = bayTracts[0].geometry.coordinates[0][0][0];
+    geoOffset = {x:-samplePoint[0],y:-samplePoint[1]};
+
+    //the manual offset
+    offset = {x:mainCanvas.width/4,y:mainCanvas.height/4};
+
+    //manually adjusting the scale to taste
+    let s = mainCanvas.width*2/5;
+    scale = {x:s,y:s*(-1)};
+
+    //drawing tract outlines to an overlay
+    tractOutlines = createFramebuffer({width:width,height:height});
+    tractOutlines.begin();
+    strokeWeight(1);
+    renderTractOutlines(geoOffset,color(100,10));
+    tractOutlines.end();
+
+    holcTexture = createFramebuffer(width,height);
+
+    holcTexture.begin();
+    renderHOLCTracts(geoOffset,oakHolcTracts);
+    renderHOLCTracts(geoOffset,sfHolcTracts);
+    renderHOLCTracts(geoOffset,sjHolcTracts);
+    holcTexture.end();
+
+    //creating map mask
+    mask.begin();
+    background(0,255);
+    renderTracts(geoOffset,() => {fill(255,255,255)});
+    mask.end();
+
+    flowField = new FlowField(mask,0,null,randomColor());
+    flowField.calculateAttractors(NUMBER_OF_ATTRACTORS);
+}
+
+function setup_Prerendered(){
+    whiteComparisonPreset = new Preset("Change in White Population",
+                                            "P<sub>White 2000</sub> - P<sub>White 2020</sub>",0,preset0Attractors,preset0Repulsors);
+    blackComparisonPreset = new Preset("Change in Black Population",
+                                            "P<sub>Black 2000</sub> - P<sub>Black 2020</sub>",1,preset1Attractors,preset1Repulsors);
+    asianComparisonPreset = new Preset("Change in Asian Population",
+                                            "P<sub>Asian 2000</sub> - P<sub>Asian 2020</sub>",2,preset2Attractors,preset2Repulsors);
+    whiteProportionComparisonPreset = new Preset("Change In Proportion of Population Identifying as White",
+                                            "P<sub>White 2000</sub> / P<sub>Total 2000</sub> - P<sub>White 2020</sub> / P<sub>Total 2020</sub>",3,preset3Attractors,preset3Repulsors);
+    blackProportionComparisonPreset = new Preset("Change In Proportion of Population Identifying as Black",
+                                            "P<sub>Black 2000</sub> / P<sub>Total 2000</sub> - P<sub>Black 2020</sub> / P<sub>Total 2020</sub>",4,preset4Attractors,preset4Repulsors);
+    asianProportionComparisonPreset = new Preset("Change In Proportion of Population Identifying as Asian",
+                                            "P<sub>Asian 2000</sub> / P<sub>Total 2000</sub> - P<sub>Asian 2020</sub> / P<sub>Total 2020</sub>",5,preset5Attractors,preset5Repulsors);
+
+    rentBurden1= new Preset("Renters spending less than 10% of monthly income","idk",9,preset6Attractors,preset6Repulsors);
+    rentBurden2= new Preset("Renters spending 10-14% of monthly income","idk",10,preset7Attractors,preset7Repulsors);
+    rentBurden3= new Preset("Renters spending 15-19% of monthly income","idk",11,preset8Attractors,preset8Repulsors);
+    rentBurden4= new Preset("Renters spending 20-24% of monthly income","idk",12,preset9Attractors,preset9Repulsors);
+    rentBurden5= new Preset("Renters spending 25-29% of monthly income","idk",13,preset10Attractors,preset10Repulsors);
+    rentBurden6= new Preset("Renters spending 30-34% of monthly income","idk",14,preset11Attractors,preset11Repulsors);
+    rentBurden7= new Preset("Renters spending 35-39% of monthly income","idk",15,preset12Attractors,preset12Repulsors);
+    rentBurden8= new Preset("Renters spending 40-49% of monthly income","idk",16,preset13Attractors,preset13Repulsors);
+    rentBurden9= new Preset("Renters spending more than 50% of monthly income","idk",17,preset14Attractors,preset14Repulsors);
+
+    presets = [
+        whiteProportionComparisonPreset,
+        blackProportionComparisonPreset,
+        asianProportionComparisonPreset,
+        whiteComparisonPreset,
+        blackComparisonPreset,
+        asianComparisonPreset,
+        rentBurden1,
+        rentBurden2,
+        rentBurden3,
+        rentBurden4,
+        rentBurden5,
+        rentBurden6,
+        rentBurden7,
+        rentBurden8,
+        rentBurden9
+    ];
+
+    //creating map mask
+    mask.begin();
+    image(presetFlowMask,-mask.width/2,-mask.height/2,mask.width,mask.height);
+    mask.end();
+    flowField = new FlowField(mask,0,null,randomColor());
+}
+
+function mousePressed(){
+    // flowField.logClosestAttractorToMouse();
+}
+
+function logPresets(){
+    let i = 0;
+    let bigString;
+    for(let preset of presets){
+        let a = getSignificantPoints(NUMBER_OF_ATTRACTORS,preset.demographicFunction);
+        let r = getLeastSignificantPoints(NUMBER_OF_ATTRACTORS,preset.demographicFunction);
+        bigString += "\nconst preset"+i+"Attractors = "+JSON.stringify(a)+";";
+        bigString += "\nconst preset"+i+"Repulsors = "+JSON.stringify(r)+";";
+        i++;
+    }
+    console.log(bigString);
+}
+
+function setup(){
+    //create canvas and grab webGL context
+    // setAttributes('antialias',false);
+
+    // pixelDensity(1);
+    mainCanvas = createCanvas(500,500,WEBGL);
+    gl = mainCanvas.GL;
+    randomShader = createShader(defaultVert,randomFrag);
+
+    mask = createFramebuffer({width:width,height:height});
+    
+    if(devMode)
+        setup_DevMode();
+    else
+        setup_Prerendered();
+
+    // saveTable(rentData2000,'CONVERTED_Tracts_by_Rent_2000.csv');
+
+    initGL();
+
+
+
+    presets[0].setActive(0,flowField);
+}
+
+function renderFlowFields(){
+    for(let i = 0; i<flowFields.length; i++){
+        image(flowFields[i].flowFieldTexture,-width/2+i*width/4,height/2-height/4,width/4,height/4);
+    }
+}
+
+function draw(){
+    flowField.updateParametersFromGui();
+    if(flowField.isActive){
+        flowField.updateParticles();
+        flowField.renderGL();
+        if(flowField.showingData)
+            flowField.renderData();
+        if(flowField.renderAs){
+            // flowField.renderAttractors();
+        }
+        if(flowField.renderRs){
+            // flowField.renderRepulsors();
+        }
+    }
+}
---
+++ gentrificationflowfield/stats.js
@@ -0,0 +1,199 @@
+function raceRatio2020to2000(tract,field){
+    if(tract.raceData2000 == undefined || tract.raceData2020 == undefined)
+        return 0;
+    const val = tract.raceData2020.obj[field]/tract.raceData2000.obj[field];
+    if(val == NaN)
+        return 0;
+    if(val == Infinity)
+        return 0;
+    if(!val)
+        return 0;
+    return val;
+}
+
+function whitePeopleComparedTo2000(tract){
+    return raceRatio2020to2000(tract,'White');
+}
+function blackPeopleComparedTo2000(tract){
+    return raceRatio2020to2000(tract,'Black');
+}
+function asianPeopleComparedTo2000(tract){
+    return raceRatio2020to2000(tract,'Asian');
+}
+
+function raceDifference(tract,field){
+    if(tract.raceData2000 == undefined || tract.raceData2020 == undefined)
+        return 0;
+    const val = tract.raceData2020.obj[field] - tract.raceData2000.obj[field];
+    if(val == NaN)
+        return 0;
+    if(val == Infinity)
+        return 0;
+    if(!val)
+        return 0;
+    return val;
+}
+
+function whitePeopleChange(tract){
+    return raceDifference(tract,'White');
+}
+function blackPeopleChange(tract){
+    return raceDifference(tract,'Black');
+}
+function asianPeopleChange(tract){
+    return raceDifference(tract,'Asian');
+}
+
+function raceChangeInProportion(tract,field){
+    if(tract.raceData2000 == undefined || tract.raceData2020 == undefined)
+        return 0;
+    const val = (tract.raceData2020.obj[field]/tract.raceData2020.obj.Total) - (tract.raceData2000.obj[field]/tract.raceData2000.obj.Total);
+    if(val == NaN)
+        return 0;
+    if(val == Infinity)
+        return 0;
+    if(!val)
+        return 0;
+    return val;
+}
+
+function proportionalWhiteChange(tract){
+    return raceChangeInProportion(tract,'White');
+}
+function proportionalBlackChange(tract){
+    return raceChangeInProportion(tract,'Black');
+}
+function proportionalAsianChange(tract){
+    return raceChangeInProportion(tract,'Asian');
+}
+
+
+function ratioWhiteChange(tract){
+    return (tract.raceData2020.obj.White/tract.raceData2020.obj.Total) / (tract.raceData2000.obj.White/tract.raceData2000.obj.Total);
+}
+function ratioBlackChange(tract){
+    if(tract.raceData2000 == undefined || tract.raceData2020 == undefined)
+        return 0;
+    let val = (tract.raceData2020.obj.Black/tract.raceData2020.obj.Total) / (tract.raceData2000.obj.Black/tract.raceData2000.obj.Total);
+    return val;
+}
+function ratioAsianChange(tract){
+    return (tract.raceData2020.obj.Asian/tract.raceData2020.obj.Total) / (tract.raceData2000.obj.Asian/tract.raceData2000.obj.Total);
+}
+
+function rentBurden(tract,field){
+    if(tract.rentData2000 == undefined || tract.rentData2020 == undefined){
+        return 0;
+    }
+    let val = tract.rentData2020.obj[field]-tract.rentData2000.obj[field];
+    if(val == NaN)
+        return 0;
+    if(val == Infinity)
+        return 0;
+    if(!val)
+        return 0;
+    return val;
+}
+function rentBurdenLessThan10(tract){
+    return rentBurden(tract,'Less than 10 percent');
+}
+function rentBurden10to14(tract){
+    return rentBurden(tract,'10 to 14 percent');
+}
+function rentBurden15to19(tract){
+    return rentBurden(tract,'15 to 19 percent');
+}
+function rentBurden20to24(tract){
+    return rentBurden(tract,'20 to 24 percent');
+}
+function rentBurden25to29(tract){
+    return rentBurden(tract,'25 to 29 percent');
+}
+function rentBurden30to34(tract){
+    return rentBurden(tract,'30 to 34 percent');
+}
+function rentBurden35to39(tract){
+    return rentBurden(tract,'35 to 39 percent');
+}
+function rentBurden40to49(tract){
+    return rentBurden(tract,'40 to 49 percent');
+}
+function rentBurden50orMore(tract){
+    return rentBurden(tract,'50 percent or more');
+}
+
+function getTopNTracts(n,func){
+    let vals = bayTracts.toSorted((a,b) => {
+        if(!a.hasData || a.raceData2020 == undefined || a.raceData2000 == undefined || a.rentData2000 == undefined || a.rentData2020 == undefined){
+            return 0;
+        }
+        else if(!b.hasData || b.raceData2020 == undefined || b.raceData2000 == undefined || b.rentData2000 == undefined || b.rentData2020 == undefined){
+            return 0;
+        }
+        let A = func(a);
+        let B = func(b);
+
+        if(A>B)
+            return -1;
+        else if(B>A)
+            return 1;
+        else
+            return 0;
+    });
+    return vals.slice(0,n);
+}
+
+function getBottomNTracts(n,func){
+    let vals = bayTracts.toSorted((a,b) => {
+    if(!a.hasData || a.raceData2020 == undefined || a.raceData2000 == undefined){
+            return 0;
+        }
+        else if(!b.hasData || b.raceData2020 == undefined || b.raceData2000 == undefined){
+            return 0;
+        }
+
+        let A = func(a);
+        let B = func(b);
+
+        if(A>B)
+            return -1;
+        else if(B>A)
+            return 1;
+        else
+            return 0;
+    });
+    //be careful to skip the tracts that don't have data, which will be at the bottom
+    for(let i = vals.length-1; i>=0; i--){
+        if(vals[i].hasData){
+            return vals.slice((i-n),i);
+        }
+    }
+}
+
+function getSignificantPoints(n,func){
+    let tracts = getTopNTracts(n,func);
+    let points = [];
+    for(let i = 0; i<min(n,tracts.length); i++){
+        let point = {
+            x:((tracts[i].centroid.x+geoOffset.x)*scale.x+offset.x)/width+0.5,
+            y:((tracts[i].centroid.y+geoOffset.y)*scale.y+offset.y)/height+0.5,
+            strength:func(tracts[i]),
+            tractName:tracts[i].properties.NAMELSAD
+        }
+        points.push(point);
+    }
+    return points;
+}
+function getLeastSignificantPoints(n,func){
+    let tracts = getBottomNTracts(n,func);
+    let points = [];
+    for(let i = 0; i<n; i++){
+        let point = {
+            x:((tracts[i].centroid.x+geoOffset.x)*scale.x+offset.x)/width+0.5,
+            y:((tracts[i].centroid.y+geoOffset.y)*scale.y+offset.y)/height+0.5,
+            strength:func(tracts[i])
+        }
+        points.push(point);
+    }
+    return points;
+}
