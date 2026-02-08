7e0ada8ef41b5269a6795d54b3bb7cb167a5f99b
alexlafetra <alexlafetrathompson@gmail.com>
2026-02-08 13:12:47 -0500

    tweaking!

---
+++ journal.js
@@ -0,0 +1,153 @@
+ const visual_art = {
+    // 2026
+    date_2026: [
+        '/images/visual/2026/star_small.png',
+        '/images/visual/2026/barbed_wire_handdrawn.png'
+    ],
+    loose_ends : [
+        '/images/visual/loose_ends/match_made_in_heaven.jpeg',
+        '/images/visual/loose_ends/third_chamber_stung.jpeg'
+    ],
+    //2025
+    date_2025: [
+        // walnut
+        '/images/visual/2025/walnut_broke_through_a_window.jpeg',
+        // roly poly
+        '/images/visual/2025/rolypoly.png'
+    ]
+}
+const online_work = {
+    thermal_printer_fighter : [
+        {
+            type : 'title',
+            content : 'thermal printer fighter'
+        },
+        {
+            type : 'link',
+            content : 'link',
+            address : 'https://alexlafetra.github.io/thermalprinterfighter/'
+        },
+        {
+            type : 'image',
+            content : '/images/thumbnails/thermalprinterfighter.gif',
+            width: 200
+        },
+        {
+            type : 'image',
+            content : '/images/thumbnails/thermal_printer_fighter.jpeg'
+        },
+        {
+            type : 'text',
+            content : 'test'
+        },
+    ],
+    draw_txt : [
+        {
+            type : 'link',
+            content : 'link',
+            address : 'https://alexlafetra.github.io/draw.txt/'
+        },
+    ],
+    confession_jpeg : [
+        {
+            type : 'link',
+            content : 'link',
+            address : 'https://alexlafetra.github.io/contradictions.jpeg/'
+        },
+
+    ],
+    liquid_png : [
+        {
+            type : 'link',
+            content : 'link',
+            address : 'https://alexlafetra.github.io/liquidpng/'
+        },
+    ],
+    flipbook:[
+        {
+            type : 'link',
+            content : 'link',
+            address : 'https://alexlafetra.github.io/flipbook/'
+        },
+    ],
+    gentrificationFlowField:[
+        {
+            type : 'link',
+            content : 'link',
+            address : 'https://alexlafetra.github.io/gentrificationflowfield/'
+        },
+
+    ]
+}
+const list = [
+    //thermal printer fighter
+    {
+        title:'thermal printer fighter',
+        thumbnail:'/images/thumbnails/thermalprinterfighter.gif',
+        section:'web',
+        onlineWork : 'thermal_printer_fighter'
+    },
+    //draw.txt
+    {
+        title:'draw.txt',
+        thumbnail:'/images/thumbnails/ascii_sketchbook.png',
+        section:'web',
+        onlineWork : 'draw_txt'
+    },
+    //confession.jpeg
+    {
+        title:'confession.jpeg',
+        thumbnail:'/images/thumbnails/confession.jpeg',
+        section:'web',
+        onlineWork : 'confession_jpeg'
+    },
+    //liquid.png
+    {
+        title:'liquid.png',
+        thumbnail:'/images/thumbnails/liquidpng.gif',
+        section:'web',
+        onlineWork : 'liquid_png'
+    },
+    //flipbook
+    {
+        title:'flipbook',
+        thumbnail:'/images/thumbnails/border_animated.gif',
+        section:'web',
+        onlineWork : 'flipbook'
+    },
+    //gentrification flow field
+    {
+        title:'gentrification flow field',
+        thumbnail:'/images/thumbnails/gentrificationflowfield.gif',
+        section:'web',
+        onlineWork : 'gentrificationFlowField'
+    },
+    {
+        title:'2026',
+        section:'visual',
+        visualArt:"date_2026"
+    },
+    {
+        title:'loose ends',
+        section:'visual',
+        visualArt:"loose_ends"
+    },
+    {
+        title:'2025',
+        section:'visual',
+        visualArt:"date_2025"
+    },
+    {
+        title:'2024',
+        section:'visual',
+        visualArt:"date_2024"
+    },
+    {
+        title:'stepchild midi sequencer',
+        section:'hardware'
+    },
+    {
+        title:'open source tamagotchi',
+        section:'hardware'
+    }
+]
